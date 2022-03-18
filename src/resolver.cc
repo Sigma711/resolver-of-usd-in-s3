abe6b66e25cb7b3b2c9ea4992857f13b5118cbbf
Sigma711 <1979934715@qq.com>
2022-03-18 02:13:32 +0000

    Develop the resolver

--- src/resolver.cc
+++ src/resolver.cc
@@ -1,1 +1,34 @@
 #include "resolver.h"
+
+#include <pxr/base/tf/fileUtils.h>
+#include <pxr/base/tf/pathUtils.h>
+#include <pxr/base/tf/registryManager.h>
+#include <pxr/base/tf/type.h>
+#include <pxr/usd/ar/asset.h>
+#include <pxr/usd/ar/assetInfo.h>
+#include <pxr/usd/ar/defaultResolver.h>
+#include <pxr/usd/ar/definePackageResolver.h>
+#include <pxr/usd/ar/defineResolver.h>
+#include <pxr/usd/ar/packageResolver.h>
+#include <pxr/usd/ar/resolverContext.h>
+#include <pxr/usd/ar/threadLocalScopedCache.h>
+#include <pxr/usd/usd/zipFile.h>
+#include <tbb/concurrent_hash_map.h>
+
+#include <memory>
+
+#include "s3.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+namespace {
+usd_s3::S3 g_s3;
+}
+
+struct S3Resolver::Cache {
+  using _PathToResolvedPathMap =
+      tbb::concurrent_hash_map<std::string, std::string>;
+  _PathToResolvedPathMap _pathToResolvedPathMap;
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
--- src/resolver.h
+++ src/resolver.h
@@ -14,6 +14,42 @@ class S3Resolver : public ArDefaultResolver {
  public:
   S3Resolver();
   ~S3Resolver();
+
+  std::string Resolve(const std::string& path);
+
+  std::string ResolveWithAssetInfo(const std::string& path,
+                                   ArAssetInfo* assetInfo);
+
+  bool IsRelativePath(const std::string& path);
+
+  VtValue GetModificationTimestamp(const std::string& path,
+                                   const std::string& resolvedPath);
+
+  void UpdateAssetInfo(const std::string& identifier,
+                       const std::string& filePath,
+                       const std::string& fileVersion, ArAssetInfo* assetInfo);
+
+  bool FetchToLocalResolvedPath(const std::string& path,
+                                const std::string& resolvedPath);
+
+  void ConfigureResolverForAsset(const std::string& path);
+
+  void RefreshContext(const ArResolverContext& context);
+
+  void BeginCacheScope(VtValue* cacheScopeData);
+
+  void EndCacheScope(VtValue* cacheScopeData);
+
+ private:
+  struct Cache;
+  using ResolveCache = ArThreadLocalScopedCache<Cache>;
+  using CachePtr = ResolveCache::CachePtr;
+
+  CachePtr GetCurrentCache();
+
+  ResolveCache cache_;
 };
+
+PXR_NAMESPACE_CLOSE_SCOPE
 
 #endif  // !SRC_S3_RESOLVER_H_
